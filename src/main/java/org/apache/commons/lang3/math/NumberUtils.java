90a6ef6ead4c522c09e46ff620e7c6deeae9fddb
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:13:11 +0800

    4108-56188-Lang2-result1575164679335

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -468,7 +468,7 @@ public class NumberUtils {
             if (hexDigits > 16) { // too many for Long
                 return createBigInteger(str);
             }
-            if (hexDigits >= 8) { // too many for an int
+            if (hexDigits > 8) { // too many for an int
                 return createLong(str);
             }
             return createInteger(str);
